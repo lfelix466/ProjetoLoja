f9c16004c8752f3708c1b9131c94c5dc6daad933
Lucas Felix <l247064@dac.unicamp.br>
2024-12-22 11:38:38 -0300

    CRUD teste realizado

---
+++ Loja/Loja/Database/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Loja.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Loja.Database
+{
+    public class Contexto : DbContext 
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }   
+
+        public DbSet<Produto> Produtos { get; set; } 
+    }
+}
---
+++ Loja/Loja/Models/Produto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Loja.Models
+{
+    [Table("Produto")]
+    public class Produto
+    {
+        [Required]
+        [Column("id")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("nome")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; } = "";
+
+        [Required]
+        [Column("preco")]
+        [Display(Name = "Preço")]
+        public float Preco { get; set; }
+
+        [Column("imagem")]
+        [Display(Name = "Imagem")]
+        public required byte[] Imagem { get; set; }
+    }
+}
